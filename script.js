265dbb8ac4463cb8e1aba866d516655c15f15f96
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-21 17:05:54 +0300

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 let squareContainer = document.querySelector(".grid-squares");
-for (let i = 0; i < 16; i++) {
-	for (let j = 0; j < 16; j++) {
-		let squareDiv = document.createElement("div");
-		squareDiv.classList.add("square-div");
-		squareContainer.appendChild(squareDiv);
+function createGrid(size) {
+	let widthAndHeight =
+		(Number(
+			window
+				.getComputedStyle(squareContainer)
+				.getPropertyValue("width")
+				.replace("px", "")
+		) -
+			2) /
+		size;
+	console.log(widthAndHeight);
+	while (squareContainer.firstChild)
+		squareContainer.removeChild(squareContainer.lastChild);
+
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
+			let squareDiv = document.createElement("div");
+			squareDiv.classList.add("square-div");
+			squareDiv.style.height = `${widthAndHeight}px`;
+			squareDiv.style.width = `${widthAndHeight}px`;
+			squareContainer.appendChild(squareDiv);
+		}
 	}
 }
-
+createGrid(16);
 squareContainer.addEventListener("mouseover", trail);
 
 function trail(e) {
 	if (e.target.className != "grid-squares")
 		e.target.style.backgroundColor = "black";
+}
+
+let newGridBtn = document.getElementById("new-grid");
+newGridBtn.addEventListener("click", newGridHandler);
+
+function newGridHandler() {
+	let size = prompt("Enter the number of squares per side (maximum 100)");
+	if (size <= 100) {
+		size = Number(size);
+		createGrid(size);
+	} else alert("Number must not exceed 100");
 }
